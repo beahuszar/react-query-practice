6653e6ebc13462eed8937ea78d64eae0ba650548
beahuszar <beata.huszar@recart.com>
2022-12-01 16:43:58 +0100

    refetch defaults

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -7,9 +7,17 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery(
+  /* isLoading remains false when finished, isFetching changes to true when cache is updated */
+  const { isLoading, data, isError, error, isFetching } = useQuery(
     "super-heroes",
-    fetchSuperHeroes
+    fetchSuperHeroes,
+    {
+      // https://www.youtube.com/watch?v=A3gN4ji5p6E&list=PLC3y8-rFHvwjTELCrPrcZlo6blLBUspd2&index=8
+      // cacheTime: 5000 // 5min is default, after this value the query is garbage collected
+      // staleTime: 30000, // 0 is default, controls when the next network request can run in the background
+      // refetchOnMount: true, // true: when data is stale, false: never, always: at every mount
+      // refetchOnWindowFocus: true, // default is true, rest is same as on mount
+    }
   );
 
   if (isLoading) {
