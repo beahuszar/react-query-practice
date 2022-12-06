4f69bccb55fbca86377967f47d5eb010cbbd001a
beahuszar <beata.huszar@recart.com>
2022-12-06 11:34:09 +0100

    parallel queries

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./components/Home.page";
+import { ParallelQueriesPage } from "./components/ParallelQueries.page";
 import { RQSuperHeroPage } from "./components/RQSuperHero";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
@@ -28,6 +29,7 @@ function App() {
             </ul>
           </nav>
           <Routes>
+            <Route path="/rq-parallel" element={<ParallelQueriesPage />} />
             <Route
               path="/rq-super-heroes/:heroId"
               element={<RQSuperHeroPage />}
