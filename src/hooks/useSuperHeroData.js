fa7f12f0b04300ddfed86c5e776a6e3d57edafeb
beahuszar <beata.huszar@recart.com>
2022-12-22 10:29:06 +0100

    Initial query data (get cache from other queries)

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 const fetchSuperHero = ({ queryKey }) => {
   const heroId = queryKey[1]; // same array as the dependency array of the useQuery hook
@@ -7,6 +7,22 @@ const fetchSuperHero = ({ queryKey }) => {
 };
 
 export const useSuperHeroData = (heroId) => {
+  const queryClient = useQueryClient(); // has access to query cache
+
   // 1st input: array of items to be cached, this way all superHeroId queries will be cashed
-  return useQuery(["super-hero", heroId], fetchSuperHero);
+  return useQuery(["super-hero", heroId], fetchSuperHero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData("super-heroes") // This is called on the listing page, from where we can navigate to the hero page.
+        ?.data?.find((hero) => hero.id === parseInt(heroId)); //  If matching data is found we can use that and 'super-hero' can be fetched in the background to see if there is any difference
+
+      if (hero) {
+        return {
+          data: hero,
+        };
+      } else {
+        return undefined;
+      }
+    },
+  });
 };
